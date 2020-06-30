54f64800b637ad6644e025315a7a739ef4e87640
ArthurOnly <m.arthur@navi.ifrn.edu.br>
2020-06-30 14:29:28 -0300

    Create authController and nodemon config

---
+++ src/controllers/auth.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { response } = require('express')
+
+const router = express.Router()
+
+router.get('/sign-in',(req,res)=>{
+    return res.json('sign-in')
+})
+
+router.get('/sign-up',(req,res)=>{
+    return res.json('sign-up')
+})
+
+module.exports = router
