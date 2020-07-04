e4670eab1460d56ac2506c3a20323759e91184b2
ArthurOnly <arthurmedeirospaiva56@gmail.com>
2020-07-04 14:24:46 -0300

    Exclude auth routes from jwt

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,9 @@
 const {verifyJwt, verifyRefreshJwt} = require('../helpers/jwt')
 
 const checkJwt = (req,res,next) => {
+    const {url : path} = req
+    if (verifyExcludedPaths(path)) return next()
+
     let token = req.headers['authorization']
     token = token ? token.slice(7,token.lenght) : null
 
@@ -13,6 +16,13 @@ const checkJwt = (req,res,next) => {
     } catch{
         res.jsonUnauthorized(null,'Invalid token')
     }
+}
+
+const verifyExcludedPaths = path => {
+    const excluedPaths = ['/auth/sign-in','/aut/sign-up']
+    const isExcluded = !!excluedPaths.find(excluded=>excluded.startsWith(path))
+
+    if (isExcluded) return true
 }
 
 module.exports = checkJwt
